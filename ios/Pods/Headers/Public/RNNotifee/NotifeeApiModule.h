00d26aac6418ef180266b920918b61046f270c6a
Helena Ford <helenaellieford@gmail.com>
2021-10-04 12:54:10 +0100

    chore: remove Pods and bump notifee version

--- ios/Pods/Headers/Public/RNNotifee/NotifeeApiModule.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/@notifee/react-native/ios/RNNotifee/NotifeeApiModule.h
--- ios/Pods/Target Support Files/Pods-NotifeeDemo-NotifeeDemoTests/Pods-NotifeeDemo-NotifeeDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NotifeeDemo_NotifeeDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_NotifeeDemo_NotifeeDemoTests
-@end
